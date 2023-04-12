4c8c859a9fd167b7a0172cc7353445b93c2dba16
GoumaNguyen <ngocmanh0806@gmail.com>
2023-04-12 22:06:15 +0700

    Create app Android

---
+++ Android_App/app/src/main/java/com/example/pbl5_shsr_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pbl5_shsr_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
